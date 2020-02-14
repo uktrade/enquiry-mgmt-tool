109a534b73500eaa9674d858d516f23536387bdc
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-02-14 14:05:04 +0000

    Update api to add new Enquiries
    New Enquiries can be added using POST request and also add tests.

--- app/enquiries/views.py
+++ app/enquiries/views.py
@@ -12,15 +12,22 @@ class EnquiryListView(APIView):
     List all enquiries.
     """
 
-    renderer_classes = (TemplateHTMLRenderer,)
+    renderer_classes = (JSONRenderer, TemplateHTMLRenderer)
 
     def get(self, request, format=None):
         enquiries = models.Enquiry.objects.all()
         serializer = serializers.EnquiryDetailSerializer(enquiries, many=True)
         return Response(
-            {"serializer": serializer.data},
-            template_name="enquiry_list.html",
+            {"serializer": serializer.data}, template_name="enquiry_list.html",
         )
+
+    def post(self, request, format=None):
+        serializer = serializers.EnquirySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class EnquiryDetail(APIView):
