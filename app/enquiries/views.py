0b1c3ee4531098a18bba83b30a45c12a88743ea3
Aaron Wilson <awwilson@uk-fvfc21hul414.uk.deloitte.com>
2020-02-26 09:41:13 +0000

    add filters tests and template tags

--- app/enquiries/views.py
+++ app/enquiries/views.py
@@ -13,6 +13,24 @@ from rest_framework.views import APIView
 from app.enquiries import models, serializers
 from app.enquiries.ref_data import EnquiryStage
 
+def filter_queryset(queryset: QuerySet, query_params: QueryDict):
+    from django.db.models import Q
+    filterable = (
+        'enquiry_stage', 'owner', 'company_name__icontains', 'enquirer_email',
+        'created__lt', 'created__gt', 'date_added_to_datahub__lt',
+        'date_added_to_datahub__gt', 'owner__user__id'
+        )
+    queryset = models.Enquiry.objects.all()
+    Qs = Q()
+    # add filters to queryset
+    for key, value in query_params.items():
+        if key in filterable and value != '':
+            for keyVal in query_params.getlist(key):
+                p = {key: keyVal}
+                # Qs.add(Q(**p), Q.OR) 
+                Qs |= Q(**p)
+    queryset = queryset.filter(Qs)
+    return queryset
 
 filter_props = {
     "enquiry_stage": "enquiry_stage__in",
@@ -78,23 +96,6 @@ class EnquiryListView(APIView):
     """
 
     renderer_classes = (JSONRenderer, TemplateHTMLRenderer)
-    def get_queryset(self):
-        filterable = (
-            'enquiry_stage', 'owner', 'company_name__icontains', 'enquirer_email',
-            'created__lt', 'created__gt', 'date_added_to_datahub__lt',
-            'date_added_to_datahub__gt', 'owner__user__id'
-            )
-        # filterable_dates = (
-        #     'created__lt', 'created__gt', 'date_added_to_datahub__lt',
-        #     'date_added_to_datahub__gt')
-        queryset = models.Enquiry.objects.all()
-        # add filters to queryset
-        for key, value in self.request.query_params.items():
-            if key in filterable and value != '':
-                p = {key: value}
-                queryset = queryset.filter(**p)
-        return queryset
-
     def get_queryset(self):
         queryset = models.Enquiry.objects.all()
         return filter_queryset(queryset, self.request.GET)
