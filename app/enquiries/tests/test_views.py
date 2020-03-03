d39b2362e34a5ae3ceb3d73e72101a04942bb8ac
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-03-03 12:49:09 +0000

    Add unit tests for detail view route

--- app/enquiries/tests/test_views.py
+++ app/enquiries/tests/test_views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.test import Client, TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -10,8 +12,19 @@ class EnquiryViewTestCase(TestCase):
         self.client = Client()
 
     def test_enquiry_list(self):
-        enquiries =  [EnquiryFactory() for i in range(5)]
+        enquiries = [EnquiryFactory() for i in range(5)]
         response = self.client.get(reverse("enquiry-list"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        serializer = response.data['serializer']
+        serializer = response.data["serializer"]
         self.assertEqual(len(serializer), len(enquiries))
+
+    def test_enquiry_detail(self):
+        enquiries = [EnquiryFactory() for i in range(5)]
+        response = self.client.get(
+            reverse("enquiry-detail", kwargs={"pk": random.choice(enquiries).id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.get(
+            reverse("enquiry-detail", kwargs={"pk": 100})
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
