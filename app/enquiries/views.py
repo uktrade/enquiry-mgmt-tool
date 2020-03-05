a90e777d85f6c9c01d8f4cb8a4bb96d1529afa89
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-03-05 12:06:39 +0000

    Add Enquiry edit view
    Add Updateview to handle enquiry updates

--- app/enquiries/views.py
+++ app/enquiries/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -18,8 +19,7 @@ class EnquiryListView(APIView):
         enquiries = models.Enquiry.objects.all()
         serializer = serializers.EnquiryDetailSerializer(enquiries, many=True)
         return Response(
-            {"serializer": serializer.data},
-            template_name="enquiry_list.html",
+            {"serializer": serializer.data}, template_name="enquiry_list.html",
         )
 
 
@@ -27,12 +27,28 @@ class EnquiryDetailView(TemplateView):
     """
     View to provide complete details of an Enquiry
     """
+
     model = models.Enquiry
     template_name = "enquiry_detail.html"
 
     def get_context_data(self, **kwargs):
-        pk = kwargs['pk']
+        pk = kwargs["pk"]
         context = super().get_context_data(**kwargs)
         enquiry = get_object_or_404(models.Enquiry, pk=kwargs["pk"])
-        context['enquiry'] = enquiry
+        context["enquiry"] = enquiry
         return context
+
+
+class EnquiryEditView(UpdateView):
+    """
+    View to provide complete details of an Enquiry
+    """
+
+    model = models.Enquiry
+    fields = "__all__"
+    template_name = "enquiry_edit.html"
+
+    def form_valid(self, form):
+        enquiry = form.save(commit=False)
+        enquiry.save()
+        return redirect("enquiry-detail", pk=enquiry.pk)
--- app/urls.py
+++ app/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('enquiries/', views.EnquiryListView.as_view(), name="enquiry-list"),
     path('enquiries/<int:pk>/', views.EnquiryDetailView.as_view(), name="enquiry-detail"),
+    path('enquiries/<int:pk>/edit', views.EnquiryEditView.as_view(), name="enquiry-edit"),
 ]
