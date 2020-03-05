09e708557c35b1e7eb15bb076effc495fc1d28ea
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-03-05 17:26:51 +0000

    Add missing redirect import

--- app/enquiries/views.py
+++ app/enquiries/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import UpdateView
 from rest_framework.renderers import TemplateHTMLRenderer
