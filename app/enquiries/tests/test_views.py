57a2e0f360f602595530b2f028a416895ff577a6
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-03-10 16:31:29 +0000

    Add unit tests for Service health check route

--- app/enquiries/tests/test_views.py
+++ app/enquiries/tests/test_views.py
@@ -88,3 +88,13 @@ class EnquiryViewTestCase(TestCase):
         updated_enquiry = model_to_dict(response.context["enquiry"])
         self.assertEqual(updated_enquiry["company_name"], enquiry["company_name"])
         self.assertNotEqual(updated_enquiry["company_name"], "")
+
+
+class ServiceHealthCheckTestCase(TestCase):
+
+    def test_service_status_healthy(self):
+        """ Ping service health check route and ensure all is good """
+        response = self.client.post(reverse("ping"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response._headers['content-type'], ('Content-Type', 'text/xml'))
+        self.assertEqual('<status>OK</status>' in str(response.content), True)
