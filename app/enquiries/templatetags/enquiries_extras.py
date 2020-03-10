5627d3f44be47576ffe08317f33c56a14685d76c
Henry Cooksley <hcooksley@deloitte.co.uk>
2020-03-10 17:05:25 +0000

    Fix branch - all except owner selected in edit view and save button

--- app/enquiries/templatetags/enquiries_extras.py
+++ app/enquiries/templatetags/enquiries_extras.py
@@ -32,3 +32,25 @@ def get_field_value(instance, field_name):
         serializer = serializers.OwnerSerializer(instance)
 
     return serializer.data[field_name]
+
+
+@register.filter
+def get_field_choices(instance, field_name):
+    fields = instance._meta.fields
+
+    target = list(filter(lambda f: f.name == field_name, fields))
+
+    return target[0].choices
+
+
+@register.filter
+def get_attribute(instance, field_name):
+    fields = instance._meta.fields
+
+    target = list(filter(lambda f: f.name == field_name, fields))
+
+    return target[0].value_from_object(instance)
+
+@register.filter
+def get_owners(instance):
+    return [str(owner) for owner in models.Owner.objects.all()]
