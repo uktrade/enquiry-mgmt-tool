b6afab6ce8843de7371e197464eba8bcac9c8e13
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-03-11 10:02:45 +0000

    Update EnquiryListView to use ListAPIView
    With this DRF provides all of the pagination functionality.
    We also provide a meta class to provide some additional metadata
    for use in the template.

--- app/enquiries/views.py
+++ app/enquiries/views.py
@@ -4,56 +4,47 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import UpdateView
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from app.enquiries import models, serializers
 
 
-class EnquiryListView(GenericAPIView):
+class PaginationWithPaginationMeta(PageNumberPagination):
+    """
+    Metadata class to add additional metadata for use in template
+    """
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'num_pages': self.page.paginator.num_pages,
+            'page_range': list(self.page.paginator.page_range),
+            'current_page': self.page.number,
+            'results': data,
+        })
+
+
+class EnquiryListView(ListAPIView):
     """
     List all enquiries.
 
-    In GET: Returns a paginated list of enquiries
-    using PageNumberPagination. This is the default
-    pagination class as set globally in settings
+    In GET: Returns a paginated list of enquiries using PageNumberPagination.
+    This is the default pagination class as set globally in settings. It is
+    also inherited via a meta class to add additional metadata required
+    for use in the template
     """
 
+    template_name = "enquiry_list.html"
     renderer_classes = (JSONRenderer, TemplateHTMLRenderer)
     serializer_class = serializers.EnquiryDetailSerializer
+    pagination_class = PaginationWithPaginationMeta
 
     def get_queryset(self):
         return models.Enquiry.objects.all()
-
-    def get(self, request, format=None):
-        enquiries = self.get_queryset()
-
-        num_pages = 1
-        if self.pagination_class:
-            page_size = self.pagination_class.page_size
-            num_pages = len(enquiries) // page_size
-            if len(enquiries) % page_size:
-                num_pages += 1
-
-        paged_queryset = self.paginate_queryset(enquiries)
-        if paged_queryset:
-            serializer = self.get_serializer(paged_queryset, many=True)
-            results = self.get_paginated_response(serializer.data)
-        else:
-            results = serializers.EnquiryDetailSerializer(enquiries, many=True)
-
-        return Response(
-            {
-                "serializer": results.data,
-                "page_range": list(range(num_pages + 1)),
-                "current_page": request.query_params.get("page", "1"),
-            },
-            template_name="enquiry_list.html",
-        )
 
 
 class EnquiryDetailView(TemplateView):
