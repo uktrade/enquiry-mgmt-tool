fe69809ced13c10388a84963559eb1a1f3f42a61
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-03-19 11:35:59 +0000

    Add helper function to perform DH requests with a timeout
    Requests to DH Api doesn't timeout by default, if for some reason
    we cannot get response then the application shouldn't wait for the
    response forever and hang up hence a sensible timeout is added.
    
    All DH requests use this helper function and throw an exception
    if there is no response before they timeout.

--- app/enquiries/common/datahub_utils.py
+++ app/enquiries/common/datahub_utils.py
@@ -1,10 +1,45 @@
 import json
+import logging
 import os
 import requests
 
 from django.conf import settings
 from mohawk import Sender
+from requests.exceptions import RequestException
 from rest_framework import status
+
+
+def dh_request(method, url, payload, request_headers=None, timeout=15):
+    """
+    Helper function to perform Data Hub request
+
+    All requests have same headers, instead of repeating in each function
+    they are added in the function. If there are any custom headers they
+    can be provided using the request_headers argument.
+
+    Each request has a timeout (default=15sec) failing which throws an
+    exception which will be captured in Sentry
+    """
+
+    if request_headers:
+        headers = request_headers
+    else:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {settings.DATA_HUB_ACCESS_TOKEN}",
+        }
+
+    try:
+        response = requests.request(
+            method, url, headers=headers, data=payload, timeout=timeout
+        )
+    except RequestException as e:
+        logging.error(
+            f"Error {e} while requesting {url}, request timeout set to {timeout} secs"
+        )
+        raise e
+
+    return response
 
 
 def dh_fetch_metadata():
@@ -42,7 +77,7 @@ def dh_fetch_metadata():
             always_hash_content=False,
         )
         response = requests.get(
-            meta_url, headers={"Authorization": sender.request_header},
+            meta_url, headers={"Authorization": sender.request_header}, timeout=10
         )
         if response.ok:
             metadata[endpoint] = response.json()
@@ -88,13 +123,9 @@ def dh_company_search(company_name):
     """
     companies = []
     url = settings.DATA_HUB_COMPANY_SEARCH_URL
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {settings.DATA_HUB_ACCESS_TOKEN}",
-    }
     payload = {"name": company_name}
 
-    response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+    response = dh_request("POST", url, json.dumps(payload))
 
     # It is not an error for us if the request fails, this can happen if the
     # Access token is invalid, consider that there are no matches
@@ -128,13 +159,9 @@ def dh_contact_search(contact_name):
     Returns list of subset of fields for each contact found
     """
     url = settings.DATA_HUB_CONTACT_SEARCH_URL
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {settings.DATA_HUB_ACCESS_TOKEN}",
-    }
     payload = {"name": contact_name}
 
-    response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+    response = dh_request("POST", url, json.dumps(payload))
 
     contacts = []
     if response.status_code != status.HTTP_200_OK:
