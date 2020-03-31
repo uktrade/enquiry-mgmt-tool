4069c48da543c424988e3786f430fea0170eaa25
Rafal Enden <git@enden.com>
2020-03-31 14:27:12 +0100

    fixup! Add data-hub-api

--- app/enquiries/e2e_tests/enquiries.test.js
+++ app/enquiries/e2e_tests/enquiries.test.js
@@ -6,4 +6,8 @@ describe('Enquiries', () => {
   it('should render the header', () => {
     cy.contains('Ready to Trade')
   })
+
+  it('should render the filters', () => {
+    cy.get('.govuk-checkboxes__input').should('have.length', 9)
+  })
 })
