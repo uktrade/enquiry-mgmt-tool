f6edbfb4b641d5952c20c8d20b183506f4bee787
Rafal Enden <git@enden.com>
2020-03-31 14:50:53 +0100

    fixup! Add data-hub-api

---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+Cypress.Commands.add('login', (next = '?', username='testuser', password='testpass') => {
+    cy.visit(`/admin/login/?next=${next}`)
+    cy.get('input[name=username]').type(username)
+    cy.get('input[name=password]').type(password)
+    cy.contains('Log in').click()
+})
