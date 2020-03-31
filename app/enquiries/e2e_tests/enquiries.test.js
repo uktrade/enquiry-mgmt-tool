b406aeaedfc771cea23edf64e42133c49ddcca95
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-03-31 23:48:04 +0100

    Create test users and sample enquiries
    Add additional test cases in detail view

--- app/enquiries/e2e_tests/enquiries.test.js
+++ app/enquiries/e2e_tests/enquiries.test.js
@@ -1,4 +1,4 @@
-describe('Enquiries', () => {
+describe('Enquiries Summary view', () => {
   before(() => {
     cy.login('/enquiries/')
   })
@@ -8,6 +8,68 @@ describe('Enquiries', () => {
   })
 
   it('should render the filters', () => {
-    cy.get('.govuk-checkboxes__input').should('have.length', 9)
+    cy.get('.govuk-checkboxes__input').should('have.length', 14)
+  })
+
+  it('should render enquiries count', () => {
+    cy.get('.big-number-of-enquiries').should('have.length', 1)
+  })
+
+  it('should have 10 enquiry titles in each page', () => {
+    cy.get('.entity-title-link').should('have.length', 10)
+  })
+
+  it('should have 10 enquiry badges list in each page', () => {
+    cy.get('.entity-badges-list').should('have.length', 10)
+  })
+})
+
+
+describe('Enquiry Detail view', () => {
+  before(() => {
+    // cy.login('/enquiries/')
+    cy.viewEnquiry(2)
+  })
+
+  it('should render the header', () => {
+    cy.contains('Ready to Trade')
+  })
+
+  it('should have one back link to summary page', () => {
+    cy.get('.govuk-back-link').should('have.length', 1)
+  })
+
+  it('should have one large heading', () => {
+    cy.get('.govuk-heading-l').should('have.length', 1)
+  })
+
+  it('should have correct company name', () => {
+    cy.contains('MATCHBOX LTD')
+  })
+})
+
+
+describe('Enquiry edit view', () => {
+  before(() => {
+    cy.login('/enquiries/')
+    cy.editEnquiry(2)
+  })
+
+  it('should render the header', () => {
+    cy.contains('Ready to Trade')
+  })
+
+  it('should have one back link to summary page', () => {
+    cy.get('.govuk-back-link').should('have.length', 1)
+  })
+
+  it('should have one large heading with expected value for this id', () => {
+    cy.get('.govuk-heading-l').should('have.length', 1)
+    cy.contains('MATCHBOX LTD')
+  })
+
+  it('should update company name', () => {
+    cy.get('input[name=company_name]').type('Matchbox Corp')
+    cy.contains('Save and return').click()
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,14 @@
-Cypress.Commands.add('login', (next = '?', username='testuser', password='testpass') => {
+Cypress.Commands.add('login', (next = '?', username='trusted_user', password='testpass') => {
     cy.visit(`/admin/login/?next=${next}`)
     cy.get('input[name=username]').type(username)
     cy.get('input[name=password]').type(password)
     cy.contains('Log in').click()
+})
+
+Cypress.Commands.add('viewEnquiry', (id) => {
+    cy.visit(`/enquiries/${id}/`)
+})
+
+Cypress.Commands.add('editEnquiry', (id) => {
+    cy.visit(`/enquiries/${id}/edit`)
 })
