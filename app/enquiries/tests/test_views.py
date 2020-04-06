2f895c197c2cbdd02fe6103e9975605deed914d9
Arun Siluvery <arunk.siluvery@gmail.com>
2020-04-06 16:48:37 +0100

    Revert "Refactor CSS into separate files using BEM naming"

--- app/enquiries/tests/test_views.py
+++ app/enquiries/tests/test_views.py
@@ -198,7 +198,7 @@ class EnquiryViewTestCase(test_utils.BaseEnquiryTestCase):
         enquiries = EnquiryFactory.create_batch(2)
         response = self.client.get(reverse("enquiry-list"), **headers)
         soup = BeautifulSoup(response.content, "html.parser")
-        enquiry_els = soup.select(".entity__list-item")
+        enquiry_els = soup.select(".entity-list-item")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         count = len(enquiry_els)
         self.assertEqual(count, len(enquiries))
@@ -631,7 +631,7 @@ class EnquiryViewTestCase(test_utils.BaseEnquiryTestCase):
         )
 
         soup = BeautifulSoup(response.content, "html.parser")
-        enquiry_els = soup.select(".entity__list-item")
+        enquiry_els = soup.select(".entity-list-item")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(enquiry_els), 1)
@@ -641,7 +641,7 @@ class EnquiryViewTestCase(test_utils.BaseEnquiryTestCase):
             reverse("enquiry-list"), {"owner__id": "UNASSIGNED"}, **headers
         )
         soup = BeautifulSoup(response.content, "html.parser")
-        enquiry_els = soup.select(".entity__list-item")
+        enquiry_els = soup.select(".entity-list-item")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(enquiry_els), 1)
@@ -684,7 +684,7 @@ class EnquiryViewTestCase(test_utils.BaseEnquiryTestCase):
         )
 
         soup = BeautifulSoup(response.content, "html.parser")
-        enquiry_els = soup.select(".entity__list-item")
+        enquiry_els = soup.select(".entity-list-item")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(enquiry_els), 1)
@@ -697,7 +697,7 @@ class EnquiryViewTestCase(test_utils.BaseEnquiryTestCase):
         )
 
         soup = BeautifulSoup(response.content, "html.parser")
-        enquiry_els = soup.select(".entity__list-item")
+        enquiry_els = soup.select(".entity-list-item")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(enquiry_els), 0)
