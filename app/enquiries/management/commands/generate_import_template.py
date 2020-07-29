0d7263b2459a65945834ddcb6389d613b6b89463
Miriam Forner <mforner13@gmail.com>
2020-07-29 15:09:04 +0100

    Format management command files with flake8

--- app/enquiries/management/commands/generate_import_template.py
+++ app/enquiries/management/commands/generate_import_template.py
@@ -1,13 +1,17 @@
-from django.core.management.base import BaseCommand, no_translations
+from django.core.management.base import BaseCommand
 from django.conf import settings
-from openpyxl import Workbook
 from app.enquiries.utils import generate_import_template
 
+
 class Command(BaseCommand):
-    help = '''this command generates a .XLSX template for users to enter enquiries
-     and manually import them as a CSVfile'''
+    help = """this command generates a .XLSX template for users to enter enquiries
+     and manually import them as a CSVfile"""
 
     def handle(self, *args, **options):
         generate_import_template(settings.IMPORT_TEMPLATE_FILENAME)
-        self.stdout.write(self.style.SUCCESS(f'Successfully generated the template file to: "{OUTPUT_FILE_NAME}"'))
-
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Successfully generated the template file to:\
+ "{settings.IMPORT_TEMPLATE_FILENAME}"'
+            )
+        )
