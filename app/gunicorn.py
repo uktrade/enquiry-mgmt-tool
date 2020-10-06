ee9ab8d8284460a945074eefa53a76825707f649
Peter Hudec <peterhudec@peterhudec.com>
2020-10-06 12:03:26 +0100

    Increase default gunicorn timeout to 5 minutes

--- app/gunicorn.py
+++ app/gunicorn.py
@@ -4,6 +4,7 @@ import os
 # Access log settings
 
 accesslog = os.environ.get("GUNICORN_ACCESSLOG", "-")
+timeout = 60 * 5
 access_log_format = os.environ.get(
     "GUNICORN_ACCESS_LOG_FORMAT",
     '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(L)s %({X-Forwarded-For}i)s',
