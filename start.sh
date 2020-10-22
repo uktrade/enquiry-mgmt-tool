865adc114565611755685f5d92f7aa31ad176da0
Harel Malka <harel@harelmalka.com>
2020-10-22 11:19:30 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
 python manage.py migrate
-# sh setup-test-fixtures.sh
+sh setup-test-fixtures.sh
 # -a to append coverage
 exec coverage run -a manage.py runserver 0.0.0.0:8001 --noreload
