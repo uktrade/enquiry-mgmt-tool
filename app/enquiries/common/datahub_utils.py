865c4b944dd78ce6e97e60658ca191cf2649bbc7
santoshdasa12345 <santosh.dasari@digital.trade.gov.uk>
2022-04-21 13:51:05 +0100

    Add logic for phone number

--- app/enquiries/common/datahub_utils.py
+++ app/enquiries/common/datahub_utils.py
@@ -294,7 +294,7 @@ def dh_contact_create(request, access_token, enquirer, company_id, primary=False
         "job_title": enquirer.job_title,
         "company": {"id": company_id},
         "primary": primary,
-        "full_telephone_number": f"{enquirer.phone_country_code} {enquirer.phone}",
+        "full_telephone_number":  f"{enquirer.phone_country_code} {enquirer.phone}" if  enquirer.phone_country_code  else enquirer.phone,
         "email": enquirer.email,
         "address_same_as_company": True,
     }
