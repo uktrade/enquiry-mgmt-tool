f4bd0b3176f37a8dd23d697bc7de808f60e81223
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-09-23 14:37:48 +0100

    Fixing flake8 error

--- app/enquiries/tests/test_views.py
+++ app/enquiries/tests/test_views.py
@@ -693,7 +693,8 @@ class EnquiryViewTestCase(test_utils.BaseEnquiryTestCase):
         self.assertEqual(len(enquiry_els), 0)
 
     def test_import_template(self):
-        """Tests that the dynamically generated .XLSX template is accessible has the correct format.
+        """
+        Tests that the dynamically generated .XLSX template is accessible has the correct format.
         The spreadsheet has multiple sheets with the 'enquiries' sheet used to capture user input.
         All other sheets are populate with the apps ref_data.py
         """
