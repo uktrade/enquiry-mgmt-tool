fba59e62b12fb5a1b98c6f08f7e657e4bcc77e10
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2022-09-28 10:34:23 +0100

    Added always_hash_content=False (#367)

--- app/enquiries/common/datahub_utils.py
+++ app/enquiries/common/datahub_utils.py
@@ -97,7 +97,11 @@ def fetch_metadata(name):
     url = os.path.join(settings.DATA_HUB_METADATA_URL, name)
     response = cached_requests.get(
         url,
-        auth=HawkAuth(id=settings.DATA_HUB_HAWK_ID, key=settings.DATA_HUB_HAWK_KEY),
+        auth=HawkAuth(
+            id=settings.DATA_HUB_HAWK_ID,
+            key=settings.DATA_HUB_HAWK_KEY,
+            always_hash_content=False,
+        ),
         timeout=10,
     )
     response.raise_for_status()
