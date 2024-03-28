6c5db80b534c00dda5e8d692d2d41af4c85aa3f3
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-03-28 09:18:16 +0000

    Test coverage

--- app/pingdom/tests/test_views.py
+++ app/pingdom/tests/test_views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from redis.exceptions import RedisError
 
+from app.pingdom.services import CheckCelery, CheckDatabase, CheckRedis
 from app.pingdom.views import ping
 
 
@@ -15,26 +16,36 @@ pytestmark = pytest.mark.django_db
 
 
 class ServiceHealthCheckPingdomTestCase(test_dh_utils.DataHubUtilsTests):
-    def test_ping(self):
-        """Fake Celery for Circle CI"""
-        with patch(
-            'app.enquiries.celery.app.control.inspect.stats',
-            return_value=[{"dummy": True}]
-        ):
-            response = ping({})
+    @patch.object(CheckDatabase, 'check')
+    @patch.object(CheckCelery, 'check')
+    @patch.object(CheckRedis, 'check')
+    def test_ping_success(self, check_database, check_celery, check_redis):
+        check_database.return_value = (True, "")
+        check_celery.return_value = (True, "")
+        check_redis.return_value = (True, "")
+
+        response = ping()
 
         assert response.status_code == status.HTTP_200_OK
         assert '<status>OK</status>' in str(response.content)
         assert response.headers['content-type'] == 'text/xml'
 
-    def test_all_good(self):
+    @patch.object(CheckDatabase, 'check')
+    def test_ping_failure(self, check_database):
+        check_database.return_value = (False, "Error message")
+        response = ping()
+
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert '<status>FALSE</status>' in str(response.content)
+        assert '<!--Error message-->' in str(response.content)
+        assert response.headers['content-type'] == 'text/xml'
+
+    @patch.object(CheckCelery, 'check')
+    def test_all_good(self, check_celery):
         """Fake Celery for Circle CI"""
-        with patch(
-            'app.enquiries.celery.app.control.inspect.stats',
-            return_value=[{"dummy": True}]
-        ):
-            url = reverse('ping')
-            response = self.client.get(url)
+        check_celery.return_value = (True, "")
+        url = reverse('ping')
+        response = self.client.get(url)
 
         assert response.status_code == status.HTTP_200_OK
         assert '<status>OK</status>' in str(response.content)
