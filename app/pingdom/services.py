1044fe131849864f523fd0b76d9cf4e5fce44a48
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-03-28 13:58:49 +0000

    Update Pingdom health check Database, Redis and Celery (#871)
    * Change health check end point to /pingdom/ping.xml.
    * Extend health check to include Celery and Redis in addition to Database.

---
+++ app/pingdom/services.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+from django.db import DatabaseError
+from redis import Redis
+
+from app.enquiries.celery import app as celery_app
+from app.enquiries.models import Enquiry
+
+
+class CheckDatabase:
+    """Check the database is up and running."""
+
+    name = "database"
+
+    def check(self):
+        """Perform the check."""
+        try:
+            Enquiry.objects.exists()
+            return True, ""
+        except DatabaseError as exception:
+            return False, f"Pingdom check Database: {exception}"
+
+
+class CheckCelery:
+    name = "celery"
+
+    def check(self):
+        try:
+            insp = celery_app.control.inspect()
+            nodes = insp.stats()
+            if not nodes:
+                raise Exception("Celery is not running")
+            return True, ""
+        except Exception as exception:
+            return False, f"Pingdom check Celery: {exception}"
+
+
+class CheckRedis:
+    name = "redis"
+
+    def check(self):
+        try:
+            redis = Redis.from_url(settings.REDIS_BASE_URL)
+            if redis.ping():
+                return True, ""
+            else:
+                return False, "Redis is not connected"
+        except Exception as exception:
+            return False, f"Pingdom check Redis: {exception}"
+
+
+services_to_check = (
+    CheckDatabase,
+    CheckCelery,
+    CheckRedis,
+)
