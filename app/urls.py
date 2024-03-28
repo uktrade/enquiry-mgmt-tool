dfedeb73ec47ddb8a89b7f718c03f9c77998fec8
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-03-28 13:58:49 +0000

    Update Pingdom health check Database, Redis and Celery (#871)
    * Change health check end point to /pingdom/ping.xml.
    * Extend health check to include Celery and Redis in addition to Database.

--- app/urls.py
+++ app/urls.py
@@ -13,32 +13,52 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from app.enquiries import views, ping
+from app.enquiries import views
+from app.pingdom.views import ping
 
 urlpatterns = [
     path("", views.EnquiryListView.as_view(), name="index"),
     path("admin/", admin.site.urls),
     path("enquiry/", views.EnquiryCreateView.as_view(), name="enquiry-create"),
     path(
-        "enquiries/template/", views.ImportTemplateDownloadView.as_view(), name="import-template",
+        "enquiries/template/",
+        views.ImportTemplateDownloadView.as_view(),
+        name="import-template",
     ),
-    path("enquiries/import", views.ImportEnquiriesView.as_view(), name="import-enquiries"),
-    path("enquiries/<int:pk>/", views.EnquiryDetailView.as_view(), name="enquiry-detail"),
-    path("enquiries/<int:pk>/edit", views.EnquiryEditView.as_view(), name="enquiry-edit"),
+    path(
+        "enquiries/import", views.ImportEnquiriesView.as_view(), name="import-enquiries"
+    ),
+    path(
+        "enquiries/<int:pk>/", views.EnquiryDetailView.as_view(), name="enquiry-detail"
+    ),
+    path(
+        "enquiries/<int:pk>/edit", views.EnquiryEditView.as_view(), name="enquiry-edit"
+    ),
     path(
         "enquiries/<int:pk>/company-search",
         views.EnquiryCompanySearchView.as_view(),
         name="enquiry-company-search",
     ),
-    path("enquiries/<int:pk>/delete", views.EnquiryDeleteView.as_view(), name="enquiry-delete"),
-    path("healthcheck/ping", ping.ping, name="ping"),
-    path("dh-adviser-search", views.DataHubAdviserSearch.as_view(), name="dh-adviser-search"),
+    path(
+        "enquiries/<int:pk>/delete",
+        views.EnquiryDeleteView.as_view(),
+        name="enquiry-delete",
+    ),
+    path("pingdom/ping.xml", ping, name="ping"),
+    path(
+        "dh-adviser-search",
+        views.DataHubAdviserSearch.as_view(),
+        name="dh-adviser-search",
+    ),
     path("api/v1/enquiries", views.APIEnquiries.as_view(), name="api-v1-enquiries"),
 ]
 
 if settings.FEATURE_FLAGS["ENFORCE_STAFF_SSO_ON"]:
-    urlpatterns.append(path("auth/", include("authbroker_client.urls")), )
+    urlpatterns.append(
+        path("auth/", include("authbroker_client.urls")),
+    )
