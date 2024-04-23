04a85fd459753ffd280acb1ddd5eef95bfbd2bb7
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-04-23 10:55:36 +0100

    DPM 202 asim formatter logging (#893)
    * Use ASIM formatter for logging on production environments
    * Upgrade Python version in Docker

--- app/settings/e2etest.py
+++ app/settings/e2etest.py
@@ -1,3 +1,36 @@
 from app.settings.common import *
 
 ROOT_URLCONF = 'app.testfixtureapi_urls'
+
+# For local/E2E tests use "plain" logging (read non ASIM formatted)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '[%(asctime)s] [%(levelname)-4s] %(name)-8s: %(message)s',
+            'datefmt': '%d-%m-%Y %H:%M:%S'
+        },
+        'file': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': 'DEBUG' if env.bool('DEBUG') else 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': '/tmp/debug.log'
+        }
+    },
+    'loggers': {
+        '': {
+            'level': 'INFO',
+            'handlers': ['console', 'file']
+        }
+    }
+}
