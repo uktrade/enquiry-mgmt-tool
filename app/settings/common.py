099c51babf70aac7b2c536977bd1aadef91164c9
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-05-07 16:18:41 +0100

    do not use sentry when dsn unset

--- app/settings/common.py
+++ app/settings/common.py
@@ -68,15 +68,15 @@ LOGGING = {
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Configure Sentry
-DJANGO_SENTRY_DSN = env('DJANGO_SENTRY_DSN')
-sentry_sdk.init(
-    dsn=DJANGO_SENTRY_DSN,
-    integrations=[
-        CeleryIntegration(),
-        DjangoIntegration(),
-    ],
-    enable_tracing=True,
-)
+if DJANGO_SENTRY_DSN := env.str('DJANGO_SENTRY_DSN', default=False):
+    sentry_sdk.init(
+        dsn=DJANGO_SENTRY_DSN,
+        integrations=[
+            CeleryIntegration(),
+            DjangoIntegration(),
+        ],
+        enable_tracing=True,
+    )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
