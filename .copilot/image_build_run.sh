54b12473eda73105c87a6a44224794d1f281c659
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-05-07 11:18:13 +0100

    add collect static command to build

---
+++ .copilot/image_build_run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run inside the container after all the other buildpacks have been applied
+
+python manage.py collectstatic --noinput
