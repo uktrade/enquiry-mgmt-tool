e86c38db38fd105dffb284ad0df6b8165e7c948b
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-05-07 16:19:09 +0100

    add required variables for local build

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -5,4 +5,8 @@ set -e
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
 
+export DJANGO_SECRET_KEY="build-time-secret"
+export DEBUG="False"
+export FEATURE_ENFORCE_STAFF_SSO_ENABLED="False"
+
 python manage.py collectstatic --noinput
