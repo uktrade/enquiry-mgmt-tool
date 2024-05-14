bf38e6d925d66a525710b6429332c4bca14d0e4b
markwarriner <mark.warriner@digital.trade.gov.uk>
2024-05-14 15:35:22 +0100

    Add extra env vars to image build.

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -10,6 +10,12 @@ export DEBUG="False"
 export FEATURE_ENFORCE_STAFF_SSO_ENABLED="False"
 export ACTIVITY_STREAM_ENQUIRY_POLL_INTERVAL_MINS="1"
 export DATABASE_CREDENTIALS='{"username": "postgres", "password": "password", "engine": "postgres", "port": 5432, "dbname": "postgres", "host": "db", "dbInstanceIdentifier": "emt-db"}'
-
+export DATA_HUB_METADATA_URL="http://docker.for.mac.localhost:8000/v4/metadsuf"
+export DATA_HUB_COMPANY_SEARCH_URL="http://docker.for.mac.localhost:8000/v4/search/company"
+export DATA_HUB_CONTACT_SEARCH_URL="http://docker.for.mac.localhost:8000/v3/search/contact"
+export DATA_HUB_CONTACT_CREATE_URL="http://docker.for.mac.localhost:8000/v3/contact"
+export DATA_HUB_INVESTMENT_CREATE_URL="http://docker.for.mac.localhost:8000/v3/investment"
+export DATA_HUB_ADVISER_SEARCH_URL="http://docker.for.mac.localhost:8000/adviser/"
+export DATA_HUB_WHOAMI_URL="http://docker.for.mac.localhost:8000/whoami/"
 
 python manage.py collectstatic --noinput
