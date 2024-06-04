7b4d3f3ef90af036f3b399b8fc24d3255f92fecf
Filippo Raimondi <105509190+PippoRaimondiDIT@users.noreply.github.com>
2024-06-04 13:36:03 +0100

    Remove celery check from pingdom (#944)
    * Remove celery check from pingdom
    
    * Remove celery pingdom checks

--- app/pingdom/tests/test_views.py
+++ app/pingdom/tests/test_views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from redis.exceptions import RedisError
 
-from app.pingdom.services import CheckCelery, CheckDatabase, CheckRedis
+from app.pingdom.services import CheckDatabase, CheckRedis
 from app.pingdom.views import ping
 
 
@@ -17,11 +17,9 @@ pytestmark = pytest.mark.django_db
 
 class ServiceHealthCheckPingdomTestCase(test_dh_utils.DataHubUtilsTests):
     @patch.object(CheckDatabase, 'check')
-    @patch.object(CheckCelery, 'check')
     @patch.object(CheckRedis, 'check')
-    def test_ping_success(self, check_database, check_celery, check_redis):
+    def test_ping_success(self, check_database, check_redis):
         check_database.return_value = (True, "")
-        check_celery.return_value = (True, "")
         check_redis.return_value = (True, "")
 
         response = ping({})
@@ -40,35 +38,12 @@ class ServiceHealthCheckPingdomTestCase(test_dh_utils.DataHubUtilsTests):
         assert '<!--Error message-->' in str(response.content)
         assert response.headers['content-type'] == 'text/xml'
 
-    @patch.object(CheckCelery, 'check')
-    def test_all_good(self, check_celery):
-        """Fake Celery for Circle CI"""
-        check_celery.return_value = (True, "")
-        url = reverse('ping')
-        response = self.client.get(url)
-
-        assert response.status_code == status.HTTP_200_OK
-        assert '<status>OK</status>' in str(response.content)
-        assert response.headers['content-type'] == 'text/xml'
-
     def test_check_database_fail(self):
         url = reverse('ping')
 
         with patch(
             'app.enquiries.models.Enquiry.objects.exists',
             side_effect=DatabaseError('No database'),
-        ):
-            response = self.client.get(url)
-
-            assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
-            assert '<status>FALSE</status>' in str(response.content)
-            assert response.headers['content-type'] == 'text/xml'
-
-    def test_check_celery_fail(self):
-        url = reverse('ping')
-        with patch(
-            'app.enquiries.celery.app.control.inspect.stats',
-            return_value=None
         ):
             response = self.client.get(url)
 
